634e586096a3a449de02aa934be7163e0bdfa841
prathamjindal <pratham2260.be23@chitkara.edu.in>
2025-07-02 15:19:20 +0530

    circular dependency added

---
+++ lec1/eventloop.js
@@ -0,0 +1,33 @@
+const fs =require("fs");
+console.log("start");
+setTimeout(()=>{
+    console.log("timer callback")
+},0)
+setImmediate(()=>{
+    console.log("Immediate callback")
+})
+function dosometask(){
+    return new Promise((resolve,reject)=>{
+        resolve("promise start")
+    })
+}
+fs.readFileSync("demo.txt",(data)=>{
+    console.log("poll phase callback");
+    setTimeout(()=>{
+        console.log("timer 2")
+    },0)
+    setImmediate(()=>{
+        console.log("immd2")
+    })
+})
+console.log("end")
+
+.catch((err)=>{
+    console.log(err)
+})
+
+process.nextTick(()=>{
+    console.log("next tick")
+})
+
+eventloop.js
---
+++ lec2/main.js
@@ -0,0 +1,5 @@
+let somefun= require("./lec2");
+console.log(somefun);
+let res1=somefun.sum(2,3);
+let res2=somefun.sub(6,5);
+console.log(res1,res2)
